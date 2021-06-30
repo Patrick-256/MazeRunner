cb0d04a589804173b0e24ba075b926a143926e00
Patrick W <321buildablock123@gmail.com>
2021-06-30 08:44:42 -0700

    First commit

---
+++ MazeRunner.js
@@ -0,0 +1,5 @@
+//2021-06-30 This is my very first attempt at programming a neural network
+//           The goal of this program is to:
+//           1. Create my own game (the maze) and each tick will present possible moves to the player (the nueral network)
+//           2. Create a neural network that will decide where to move to based in its current location and possible options to move to
+//           3. Create some sort of learning algorigthm that will modify the neural network to make it learn
